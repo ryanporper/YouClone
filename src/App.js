12e4f9c021e1b9267f123c01403c90bfa0170032
ryanporper <ryan_porper@aol.com>
2023-01-14 01:09:33 -0500

    push routes, mui, componets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+// imports placed in index.js to clean up look
+import { NavBar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000'}}>
+            <NavBar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" exact element={<VideoDetail />} />
+                <Route path="/channel/:id" exact element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default app

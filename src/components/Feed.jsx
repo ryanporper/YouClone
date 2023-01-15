1a7b3637e4014a97c03a76ad4b52acbfb4d6a4de
ryanporper <ryan_porper@aol.com>
2023-01-15 01:20:44 -0500

    small typo

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -22,7 +22,7 @@ const Feed = () => {
         />
 
         <Typography className='copyright' variant='"body2' sx={{ mt: 1.5, color: ' #fff' }}>
-          Copyright 2022 YouClone
+          Copyright © 2023 YouClone
         </Typography>
       </Box>
 

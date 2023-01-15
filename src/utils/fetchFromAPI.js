ceceb3c3cf8ce5bfb8b4766a5586987798abd102
ryanporper <ryan_porper@aol.com>
2023-01-15 01:11:18 -0500

    key update

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,9 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    // '76589c97f8msh6251dc4843f3462p150fb9jsnb44f69e8b108'
-    // process.env.RAPID_API_KEY will not work with env file keeps saying unidentified
-    "X-RapidAPI-Key": "76589c97f8msh6251dc4843f3462p150fb9jsnb44f69e8b108",
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
